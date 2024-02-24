4bc361e26f313c910a0514d9535374c770ce3bd2
hyunseok86 <darktree86@gmail.com>
2024-02-24 13:29:32 +0900

    commit

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/JASPER/workspace/JASPER_WIN_DEMON
-# Build directory: C:/JASPER/workspace/JASPER_WIN_DEMON/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/pugixml-build")
--- build/_deps/pugixml-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/JASPER/workspace/JASPER_WIN_DEMON/build/_deps/pugixml-src
-# Build directory: C:/JASPER/workspace/JASPER_WIN_DEMON/build/_deps/pugixml-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
